b6da6014f26a3ee87c6236971222d77495b79b1d
Jewel Carrier-Davis <jujicb@gmail.com>
2022-04-25 14:34:43 -0400

    Added Spotify Github as submodule (the right way)

---
+++ spotify.js
@@ -0,0 +1,46 @@
+/**
+ * NOT MY CODE; got from Spotify Github link: https://github.com/spotify/web-api-auth-examples/blob/master/client_credentials/app.js
+ * 
+ * This is an example of a basic node.js script that performs
+ * the Client Credentials oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
+
+ var request = require('request'); // "Request" library
+
+ var client_id = 'd9ab1dd64f9340cea580059be1dff6f5'; // Your client id
+ var client_secret = '21d259c05a2e474c90b7a5218e9ea90f'; // Your secret
+ 
+ // your application requests authorization
+ var authOptions = {
+   url: 'https://accounts.spotify.com/api/token',
+   headers: {
+     'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+   },
+   form: {
+     grant_type: 'client_credentials'
+   },
+   json: true
+ };
+ 
+ request.post(authOptions, function(error, response, body) {
+   if (!error && response.statusCode === 200) {
+ 
+     // use the access token to access the Spotify Web API
+     var token = body.access_token;
+     var options = {
+       url: 'https://api.spotify.com/v1/users/jmperezperez',
+       headers: {
+         'Authorization': 'Bearer ' + token
+       },
+       json: true
+     };
+     request.get(options, function(error, response, body) {
+       console.log(body);
+     });
+   }
+ });
+
